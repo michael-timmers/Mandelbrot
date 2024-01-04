5a6606ad7ec7bd34fe68680be62fca4b97308ea5
MichaelTimmers6 <97582970+MichaelTimmers6@users.noreply.github.com>
2024-01-04 14:40:29 +1000

    tried -O2 flag and time ~400ms

--- src/build_mac.sh
+++ src/build_mac.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo compiling and linking...
-time clang++ -O1 *.cpp -o MacBuild -std=c++17 -l SDL2 2> errors
+time clang++ -O2 *.cpp -o MacBuild -std=c++17 -l SDL2 2> errors
 
 #check if errors is empty
 if [ -s errors ]; then
