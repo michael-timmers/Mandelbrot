7bce17d68547efed809d871b4332f8769cdfd8f2
MichaelTimmers6 <97582970+MichaelTimmers6@users.noreply.github.com>
2024-01-24 12:16:16 +1000

    removed timer files

--- src/utils.cpp
+++ src/utils.cpp
@@ -23,43 +23,18 @@ int init() {
 }
 
 void run(bool runProfiling, int numLoops) {
-    // profiling variables:
-    int mandelbrotTimeSum = 0, renderTimeSum = 0, i = 0, mandelbrotTime, renderTime;
+    int i = 0;
 
     // event handler loops until input is given, then things are updated..
     do {
-        // renderer::clear();
-
-        timer::start();
-        // renderer::renderLegacyMandelbrot(mandelbrot::scale, mandelbrot::lowerXBound, mandelbrot::lowerYBound);
         renderer::renderMandelbrot(mandelbrot::scale, mandelbrot::lowerXBound, mandelbrot::lowerYBound);
-        timer::stop();
 
-        mandelbrotTime = timer::result().count() / 1000000;
-
-        timer::start();
         renderer::updateWindowSurfaceWithCanvas();
-        // renderer::present();
-        timer::stop();
 
-        renderTime = timer::result().count() / 1000000;
-
-        std::cout << "Time:" << mandelbrotTime << " and " << renderTime << "ns\n";
-
-        if (runProfiling) {
-            i++;
-            mandelbrotTimeSum += mandelbrotTime;
-            renderTimeSum += renderTime;
-            if (i == numLoops) {
-                std::cout << "average time:" << mandelbrotTimeSum / numLoops << " and " << renderTimeSum / numLoops << "ms"
-                          << "\n";
-                mandelbrotTimeSum = 0, renderTimeSum = 0;
-                break;
-            }
-        }
+        i++;
 
         SDL_Delay(10);
-    } while (runProfiling || eventHandler::handleInput());
+    } while ((runProfiling && i < numLoops) || eventHandler::handleInput());
 }
 
 void kill() {
