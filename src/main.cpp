d9bc6ed92f630bafad4d83782c50334177c35535
MichaelTimmers6 <97582970+MichaelTimmers6@users.noreply.github.com>
2024-04-14 16:08:17 +1000

    Fix seg fault, but have one at end now...

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,22 @@
 
-#include "utils.hpp"
+#include "appmanager.hpp"
 
 int main(void) {
-    utils::intro();  // just messages
+    AppManager app;
+
+    app.intro();  // just messages
 
     // check if every SDL object was initialised correctly.
-    if (utils::init() == 0) {
+    if (app.init() == 0) {
         // the rendering loop, pass true for limited number of renders
         // also pass num    ber of loops
-        utils::run(false, 1);
+        app.run(false, 1);
 
         // otherwise generate an image
-        // utils::saveAsPng();
+        // app.saveAsPng();
     }
 
-    utils::kill();
+    app.kill();
 
     return 0;
 }
